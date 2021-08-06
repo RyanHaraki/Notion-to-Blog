faac1b663f25a7811f244eaabe5bcb92e3d948f3
Ryan <ryan.haraki@hotmail.com>
2021-08-06 02:08:34 -0700

    basic rendering engine complete

---
+++ components/Text.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Text = ({ text, style }) => {
+  if (!text) {
+    return null;
+  }
+  return text.map((val) => {
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+      text,
+    } = val;
+
+    return (
+      <span
+        className={[
+          bold ? "font-bold" : "",
+          code
+            ? "font-mono text-sm p-1 rounded-sm text-red-400 bg-gray-100"
+            : "",
+          italic ? "font-italic" : "",
+          strikethrough ? "font-strikethrough" : "",
+          underline ? "underline" : "",
+          style ? style : "",
+        ].join(" ")}
+      >
+        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+      </span>
+    );
+  });
+};
+
+export default Text;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,41 @@
-import { getBlogs } from "../lib/notion";
+import { getDatabase, getPage, getBlocks } from "../lib/notion";
+import Link from "next/link";
+import Text from "../components/Text";
+import slugify from "slugify";
+
+export const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home({ posts }) {
-  console.log(posts[0].properties.Name.title[0].text.content);
-
   return (
     <div className="">
       {posts.map((post) => {
-        return <p>{post.properties.Name.title[0].text.content}</p>;
+        return (
+          <div key={post.id}>
+            <a href={`/post/${post.id}`}>
+              <Text text={post.properties.Name.title}>
+                {post.properties.Name.title[0].plain_text}
+              </Text>
+            </a>
+
+            {/* <Link
+              href={`/post/${slugify(
+                post.properties.Name.title[0].plain_text
+              )}`}
+            >
+            
+            </Link> */}
+          </div>
+        );
       })}
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const posts = getBlogs();
-
+  const database = await getDatabase(databaseId);
   return {
     props: {
-      posts: await posts,
+      posts: database,
     },
   };
 };
